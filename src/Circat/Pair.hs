8f79c1ca04762f68a7a00293689e68a163350beb
Conal Elliott <conal@conal.net>
2014-04-09 11:36:16 -0700

    Drop dependency on functor-combo (broken with GHC 7.8.1). Include uniform Pair functor and a few instances.

--- src/Circat/Pair.hs
+++ src/Circat/Pair.hs
@@ -22,19 +22,62 @@ module Circat.Pair (Pair(..),PairCat(..),inPair) where
 
 -- TODO: consider using standard names like fst, snd & curry.
 
+import Data.Monoid (Monoid(..))
 import Control.Arrow (arr,Kleisli)
+import Data.Functor ((<$>))
+import Data.Foldable (Foldable(..))
+import Data.Traversable (Traversable(..))
+import Control.Applicative (Applicative(..),liftA2)
+import Data.Typeable (Typeable)
+import Data.Data (Data)
 
-import FunctorCombo.Pair (Pair(..))
+-- import FunctorCombo.Pair (Pair(..))
 
 import Circat.Misc ((:*),(<~))
 import Circat.Category -- (ProductCat(..))
 import Circat.State (pureState,StateFun,StateExp)
 
--- From FunctorCombo.Pair:
--- 
---   data Pair a = a :# a deriving (Functor,Eq,Show,Typeable,Data)
--- 
--- plus many more instances
+{--------------------------------------------------------------------
+    Pair representation and basic instances
+--------------------------------------------------------------------}
+
+-- Stolen from FunctorCombo.Pair in functor-combo. Fix that package for GHC 7.8,
+-- and use it here.
+
+infixl 1 :#
+-- | Uniform pairs
+data Pair a = a :# a deriving (Functor,Eq,Show,Typeable,Data)
+
+-- The derived foldMap inserts a mempty (in GHC 7.0.4).
+instance Foldable Pair where
+  foldMap f (a :# b) = f a `mappend` f b
+
+instance Applicative Pair where
+  pure a = a :# a
+  (f :# g) <*> (a :# b) = (f a :# g b)
+
+instance Monad Pair where
+  return = pure
+  m >>= f = joinP (f <$> m)
+
+joinP :: Pair (Pair a) -> Pair a
+joinP ((a :# _) :# (_ :# d)) = a :# d
+
+-- so
+--
+--   (a :# b) >>= f = (c :# d)
+--    where
+--      (c :# _) = f a
+--      (_ :# d) = f b
+
+instance Traversable Pair where
+  traverse h (fa :# fb) = liftA2 (:#) (h fa) (h fb)
+  -- sequenceA (fa :# fb) = liftA2 (:#) fa fb
+
+{--------------------------------------------------------------------
+    
+--------------------------------------------------------------------}
+
 
 class ProductCat k => PairCat k where
   toPair :: (a :* a) `k` Pair a
