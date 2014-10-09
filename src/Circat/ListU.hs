b0b99aa0cb4e9826324c63880704f0633198bf06
Conal Elliott <conal@conal.net>
2014-10-09 11:32:50 -0700

    instances

--- src/Circat/ListU.hs
+++ src/Circat/ListU.hs
@@ -3,7 +3,7 @@
 #define AsGADT
 
 {-# LANGUAGE TypeOperators, TypeFamilies, Rank2Types #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, TupleSections #-}
 #ifdef AsGADT
 {-# LANGUAGE GADTs #-}
 #else
@@ -32,6 +32,9 @@ module Circat.ListU where
 
 -- TODO: explicit exports
 
+import Data.Monoid (Monoid(..))
+import Control.Arrow (first,second)
+import Control.Applicative (Applicative(..))
 import Data.List (unfoldr)
 
 #ifdef ListRep
@@ -60,3 +63,57 @@ instance HasRep [a] where
   abst = unListU
 #endif
 
+instance Monoid (ListU a) where
+  mempty = UnfoldR (const Nothing) undefined
+  UnfoldR f s0 `mappend` UnfoldR g t0 = UnfoldR h u0
+   where
+     u0 = Left s0
+     h (Left  s) = case f s of
+                     Nothing     -> h (Right t0)
+                     Just (a,s') -> Just (a, Left s')
+     h (Right t) = case g t of
+                     Nothing     -> Nothing
+                     Just (a,t') -> Just (a, Right t')
+
+-- Alternatively,
+--
+--      h (Left  s) = maybe (h (Right t0)) (Just . second Left) (f s)
+--      h (Right t) = (fmap.second) Right (g t)
+
+instance Functor ListU where
+  fmap f (UnfoldR h s0) = UnfoldR ((fmap.fmap.first) f h) s0
+
+--   UnfoldR (\ s -> case h s of
+--                     Nothing -> Nothing
+--                     Just (a,s') -> Just (f a, s')) s0
+
+-- Synchronous version, i.e., homomorphic with ZipList rather than []
+
+-- #define LikeZipList
+
+#ifdef LikeZipList
+
+instance Applicative ListU where
+  pure a = UnfoldR (\ s -> Just (a,s)) ()
+  UnfoldR hf sf0 <*> UnfoldR hx sx0 = UnfoldR h (sf0,sx0)
+   where
+     h (sf,sx) = case (hf sf, hx sx) of
+                   (Just (f,sf'), Just (x,sx')) -> Just (f x, (sf',sx'))
+                   _                            -> Nothing
+
+#else
+
+instance Applicative ListU where
+  pure a = UnfoldR (\ done -> if done then Nothing else Just (a,True)) False
+  UnfoldR hf sf0 <*> UnfoldR hx sx0 = UnfoldR h (sf0,sx0)
+   where
+     h (sf,sx) = case (hf sf, hx sx) of
+                   (Just (f,_  ), Just (x,sx')) -> Just (f x, (sf,sx'))
+                   (Just (_,sf'), Nothing)      -> h (sf',sx0)
+                   _                            -> Nothing
+
+-- Note the recursion in h, which could be removed as in the "nothing at all"
+-- paper. Note also the redundant application of hf sf, which could perhaps be
+-- cached in the state if we could save functions.
+
+#endif
